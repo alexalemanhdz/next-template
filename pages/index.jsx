9d9aaf9a956082a6c7a2d488b172dbb5876621d5
alex@springlabs <alex.aleman.public@gmail.com>
2020-04-02 18:40:46 -0600

    updated eslint and prettier conf files

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,31 +3,31 @@ import React, { type Node } from 'react';
 // import withRedux from 'lib/redux';
 
 type Props = {
-	children: Node,
-	title?: string,
+    children: Node,
+    title?: string,
 };
 
 function HomePage({ children, title = 'Hello World' }: Props) {
-	return (
-		<div>
-			<p>{title}</p>
-			<style global jsx>
-				{`
-					* {
-						margin: 0;
-						padding: 0;
-					}
+    return (
+        <div>
+            <p>{title}</p>
+            <style global jsx>
+                {`
+                    * {
+                        margin: 0;
+                        padding: 0;
+                    }
 
-					body,
-					html,
-					#__next {
-						height: 100%;
-						width: 100%;
-					}
-				`}
-			</style>
-		</div>
-	);
+                    body,
+                    html,
+                    #__next {
+                        height: 100%;
+                        width: 100%;
+                    }
+                `}
+            </style>
+        </div>
+    );
 }
 
 export default HomePage;
--- redux/store.js
+++ redux/store.js
@@ -1,18 +1,18 @@
 import { createStore } from 'redux';
 
 const tempState = {
-	// add your states here
+    // add your states here
 };
 
 const reducer = (state = tempState, action) => {
-	switch (action.type) {
-		default:
-			return state;
-	}
+    switch (action.type) {
+        default:
+            return state;
+    }
 };
 
 const initializeStore = initialState => {
-	return createStore(reducer, initialState);
+    return createStore(reducer, initialState);
 };
 
 export default initializeStore;
